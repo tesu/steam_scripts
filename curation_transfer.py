8bb9c6805f9e2759b2276daae729aa2519904e22
3edgy6u <3edgy6u@gmail.com>
2016-04-28 18:32:52 -0400

    basically perfect now

--- curation_transfer.py
+++ curation_transfer.py
@@ -1,3 +1,4 @@
+import sys
 import pickle
 import re
 from selenium import webdriver
@@ -9,6 +10,9 @@ GROUP_ONE = 'isis-official'
 GROUP_TWO = 'isis-official2'
 TRANSFERS = 1
 
+if len(sys.argv) > 1:
+    TRANSFERS = int(sys.argv[1])
+
 driver = webdriver.Chrome()
 driver.get('https://steamcommunity.com/');
 try:
@@ -19,14 +23,25 @@ try:
 except FileNotFoundError:
     pickle.dump(driver.get_cookies(), open("cookies.pkl","wb"))
 
-driver.get('https://steamcommunity.com/login/home/')
 
-WebDriverWait(driver, 600).until(ec.presence_of_element_located((By.CSS_SELECTOR, '.username')))
+driver.get('https://steamcommunity.com/groups/'+GROUP_ONE+'#curation')
+if len(driver.find_elements_by_css_selector('.username')) == 0:
+    driver.get('https://steamcommunity.com/login/home/')
+    WebDriverWait(driver, 600).until(ec.presence_of_element_located((By.CSS_SELECTOR, '.username')))
+    driver.get('https://steamcommunity.com/groups/'+GROUP_ONE+'#curation')
+
+def green_button_exists(self):
+    if len(driver.find_elements_by_css_selector('.btn_green_white_innerfade.btn_medium span')) > 1:
+        return True
+    return False
+
+def gray_button_exists(self):
+    if len(driver.find_elements_by_css_selector('.btn_grey_white_innerfade.btn_medium span')) > 0:
+        return True
+    return False
 
 curations = []
 for i in range(0, TRANSFERS):
-    driver.get('https://steamcommunity.com/groups/'+GROUP_ONE+'#curation')
-
     WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.CSS_SELECTOR, '.RecommendedApps_paging_pagelink')))
     check = driver.find_elements_by_css_selector('.curation_app_block_name') [-1].text
     driver.find_elements_by_css_selector('.RecommendedApps_paging_pagelink') [-1].click()
@@ -42,17 +57,19 @@ for i in range(0, TRANSFERS):
     appid, app = re.match(r"^Curator_DeleteRecommendation\('"+GROUP_ONE+r"',(\d+),"+r'"(.*)"\);',delete.get_attribute('onclick')).group(1, 2)
     curations.append({'app': app, 'appid': appid, 'blurb': blurb})
 
+    print('Grabbed {0}\'s curation: {1}'.format(curations[i]['app'], curations[i]['blurb']), flush=True)
+
     delete.click()
-    def button_exists(self):
-        if len(driver.find_elements_by_css_selector('.btn_green_white_innerfade.btn_medium span')) > 1:
-            return True
-        return False
-    WebDriverWait(driver, 10).until(button_exists)
+    WebDriverWait(driver, 10).until(green_button_exists)
     driver.find_elements_by_css_selector('.btn_green_white_innerfade.btn_medium span') [1].click()
 
-for curation in curations:
-    driver.get('https://steamcommunity.com/groups/'+GROUP_TWO+'#curation/new')
+    if i < TRANSFERS-1:
+        WebDriverWait(driver, 10).until(gray_button_exists)
+        driver.find_elements_by_css_selector('.btn_grey_white_innerfade.btn_medium span') [0].click()
 
+driver.get('https://steamcommunity.com/groups/'+GROUP_TWO+'#curation/new')
+
+for curation in curations:
     WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.ID, 'curationAppInput')))
     driver.find_element_by_id('curationAppInput').send_keys(curation['app'])
     driver.execute_script('document.getElementById("curationAppIDInput").style.display = "block"')
@@ -60,7 +77,11 @@ for curation in curations:
     driver.find_element_by_id('curationBlurbInput').send_keys(curation['blurb'])
     driver.find_element_by_partial_link_text('Post Recommendation').click()
 
-    print('Successfully transferred {0}\'s curation: {1}'.format(curation['app'], curation['blurb']))
+    print('Successfully transferred {0}\'s curation: {1}'.format(curation['app'], curation['blurb']), flush=True)
+
+    if curations.index(curation) < TRANSFERS-1:
+        WebDriverWait(driver, 10).until(green_button_exists)
+        driver.find_elements_by_css_selector('.btn_green_white_innerfade.btn_medium span') [0].click()
 
 pickle.dump(driver.get_cookies(), open("cookies.pkl","wb"))
 driver.quit()
