9fb6f351ebaa5e666f55cab8ec7593591ad56dde
3edgy6u <3edgy6u@gmail.com>
2016-04-28 11:15:16 -0400

    reduced reliance on time.sleep()

--- curation_transfer.py
+++ curation_transfer.py
@@ -2,6 +2,9 @@ import time
 import pickle
 import re
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 GROUP_ONE = 'isis-official'
 GROUP_TWO = 'isis-official2'
@@ -19,17 +22,15 @@ except FileNotFoundError:
 
 driver.get('https://steamcommunity.com/login/home/')
 
-if len(driver.find_elements_by_link_text('login')) > 0:
-    while len(driver.find_elements_by_link_text('login')) > 0:
-        time.sleep(1)
+if len(driver.find_elements_by_css_selector('.username')) == 0:
+    WebDriverWait(driver, 600).until(ec.presence_of_element_located((By.CSS_SELECTOR, '.username')))
     pickle.dump(driver.get_cookies(), open("cookies.pkl","wb"))
 
 curations = []
 for i in range(0, TRANSFERS):
     driver.get('https://steamcommunity.com/groups/'+GROUP_ONE+'#curation')
 
-    while len(driver.find_elements_by_css_selector('.RecommendedApps_paging_pagelink')) == 0:
-        time.sleep(1)
+    WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.CSS_SELECTOR, '.RecommendedApps_paging_pagelink')))
     driver.find_elements_by_css_selector('.RecommendedApps_paging_pagelink') [-1].click()
     time.sleep(2)
     driver.find_elements_by_css_selector('.curation_app_block_name') [-1].click()
@@ -47,12 +48,13 @@ for i in range(0, TRANSFERS):
 for curation in curations:
     driver.get('https://steamcommunity.com/groups/'+GROUP_TWO+'#curation/new')
 
-    while len(driver.find_elements_by_id('curationAppInput')) == 0:
-        time.sleep(1)
+    WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.ID, 'curationAppInput')))
     driver.find_element_by_id('curationAppInput').send_keys(curation['app'])
     driver.execute_script('document.getElementById("curationAppIDInput").style.display = "block"')
     driver.find_element_by_id('curationAppIDInput').send_keys(curation['appid'])
     driver.find_element_by_id('curationBlurbInput').send_keys(curation['blurb'])
     driver.find_element_by_partial_link_text('Post Recommendation').click()
+
+    print('Successfully transferred {0}\'s curation: {1}'.format(curation['app'], curation['blurb']))
 
 driver.quit()
